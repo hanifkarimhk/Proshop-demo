c0ef455d850a4afb62c37dd7811ce15ea9249d85
hanifkarimhk <karimhanif333@gmail.com>
2024-09-27 16:27:14 +0500

    test

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
+
+
+
+const port = process.env.PORT || 5000;
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Worldsssssssssssss!')
+});
+app.get('/api/products', async(req, res) => {
+   res.json(products);
+})
+app.get('/api/products/:id', async(req, res) => {
+   const product = products.find((p) => p._id === req.params.id);
+   res.json(product)
+ })
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,26 @@
-import {Row, Col} from 'react-bootstrap'
-import Product from '../components/Product'
-import products from '../products'
+import { useEffect, useState } from 'react';
+import {Row, Col} from 'react-bootstrap';
+import axios, { Axios } from 'axios';
+import Product from '../components/Product';
+
 
 const HomeScreen = () => {
+   const [products, setProducts] = useState([]);
+    
+
+   useEffect(()=>{
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    
+    setProducts(data);
+   };
+
+ fetchProducts();
+  }, []);
+
+   
+
   return (
     <>
         <h1>Latest Products </h1>
